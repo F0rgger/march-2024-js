490844a5f54cef951daec12d759b26c775b8ed1e
F0rgger <maxforger@gmail.com>
2024-04-29 22:21:31 +0300

    HW_1

---
+++ HomeWork/HW_1/HW_1.js
@@ -0,0 +1,40 @@
+
+let hello = 'hello';
+let school = 'owu';
+let domain1 = 'com';
+let domain2 = 'ua';
+let number1 = 1;
+let number2 = 10;
+let number3 = -999;
+let number4 = 123;
+let pi = 3.14;
+let euler = 2.7;
+let age = 16;
+let isTrue = true;
+let isFalse = false;
+
+console.log(hello, school, domain1, domain2, number1, number2, number3, number4, pi, euler, age, isTrue, isFalse);
+
+let firstName = "Сорокін";
+let middleName = "Максим";
+let lastName = "Ігорович";
+
+let person = firstName + " " + middleName + " " + lastName;
+
+console.log(person);
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+let firstName1 = prompt("Введіть ваше ім'я");
+let middleName1 = prompt("Введіть ваше по-батькові");
+let age1 = prompt("Введіть ваш вік");
+
+console.log("Ім'я:", firstName1);
+console.log("По-батькові:", middleName1);
+console.log("Вік:", age1);
